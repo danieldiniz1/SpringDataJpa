9b9560960c926787c9b91ee1c285764142c477cb
Daniel Diniz <daniel.diniz1@icloud.com>
2022-02-15 16:05:51 -0300

    adicionando primeira entidade 'Cargo'

---
+++ src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -0,0 +1,28 @@
+package br.com.alura.spring.data.orm;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cargos")
+public class Cargo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+
+    public Cargo() {
+    }
+
+    public Cargo(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
